7486b56b2ce7add3ff0d4737625d6d03e5c167d7
Erik P <122167559+EriktheRDev@users.noreply.github.com>
2023-03-23 08:50:24 -0400

    Update pop_code.py

--- pop_code.py
+++ pop_code.py
@@ -1,1 +1,15 @@
-#
+class Pop():
+  def __init__(self, layout, window, queries):
+    self.layout = layout
+    self.window = window
+    self.queries = queries
+  def appqry(self, query):
+    self.queries.append(query)
+  def readqry(self, queries):
+    return self.queries
+  
+pop_layout = [[sg.Input(), sg.Button("SAVE") [sg.Button("OPEN")]]
+pop_query = []
+
+pop_init = Pop(pop_layout, sg.Window("Pop", pop_layout), pop_query)
+
