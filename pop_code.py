55cdcd366e978bf2a7c9d000b0e42438ef4403a2
Erik P <122167559+EriktheRDev@users.noreply.github.com>
2023-03-23 12:33:28 -0400

    Update pop_code.py

--- pop_code.py
+++ pop_code.py
@@ -1,31 +1,25 @@
-class Pop():
-  def __init__(self, layout, window, queries):
-    self.layout = layout
-    self.window = window
-    self.queries = queries
-  def appqry(self, query):
-    self.queries.append(query)
-  def readqry(self, queries):
-    return self.queries
-  
-pop_layout = [[sg.Input(), sg.Button("SAVE") [sg.Button("OPEN")]]
+import PySimpleGUI as sg
+pop_layout = [[sg.Input(), sg.Button("SAVE")], [sg.Button("OPEN")]]
 pop_win = sg.Window("Pop", pop_layout)
 pop_query = []
-
-pop_init = Pop(pop_layout, pop_win, pop_query)
 
 while True:
     event, values = pop_win.read()
     if event == sg.WIN_CLOSED:
         break
     if event == "SAVE":
-        pop_init.addqry(values[0])
+        pop_query.append(values[0])
     if event == "OPEN":
-        query_inventory = pop_init.readqry(values[0])
-        pop_sub_layout = [[]]
-        for query in query_inventory:
-            query_inventory[0].append(sg.Text(query))
-        pop_sub_window = sg.Window("Past Queries [from this session]:", layout)
+        sublayout = [[]]
+        for i in pop_query:
+          sublayout[0].append(i)
+          previous = ""
+          for k in sublayout[0]: 
+            previous += str(k)
+            previous += "\n"
+        sg.Popup("Previous Queries", previous)
+        del sublayout
+        del previous
               
               
 pop_win.close()
